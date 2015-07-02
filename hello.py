bf43e046c1bfb98ff3fa9e3e715fde917b9e405b
tzhangBlast <vlsi1217@gmail.com>
2015-07-02 16:42:49 -0700

    Make some important changes to hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 1. init hello.py, git add to staging, then commit the staged snapshot.
 2. learning "Viewing Old commits": Make some important changes to hello.py
+3. Make some important changes to hello.py
+	print "hello world";
+	for i is 1:100:
+		print i
+	end
+
